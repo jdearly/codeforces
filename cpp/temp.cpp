302cebe39563059b18ae42624a5b9fedee92e22a
Josh Early <josh.early@protonmail.com>
2023-08-01 18:34:21 -0400

    add ll to template

--- cpp/temp.cpp
+++ cpp/temp.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
 
+#define ll long long
+
 using namespace std;
  
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     // solution
-     
+    
     return 0;
 }
